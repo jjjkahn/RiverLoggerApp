6a42791b401f50cc84c0b9e07959bf2154f186d5
Jason Kahn <jjjkahn@gmail.com>
2024-05-07 21:33:11 -0400

    added unit testing project and added first unit test

---
+++ RiverLoggerApi.Test/Controllers/UserControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using RiverLoggerApi.Controllers;
+using RiverLoggerApi.Models;
+using RiverLoggerApi.Services;
+
+namespace RiverLoggerApi.Test.Controllers
+{
+    public class UserControllerTests
+    {
+        private readonly Mock<IUserService> _userServiceMock;
+        public UserControllerTests()
+        {
+            _userServiceMock = new Mock<IUserService>();
+        }
+        [Fact]
+        public void GivenACalltoCreateUser_WhenAUserIsPassed_ThenUserServiceShouldBeCalledOnce()
+        {
+            //Arrage
+            var user = GeAUser();
+            _userServiceMock.Setup(x => x.Create(user)).Verifiable();
+            var uc = new UserController(_userServiceMock.Object);
+
+            //Act
+            uc.CreateUser(user);
+
+            //Assert
+            _userServiceMock.Verify(x=>x.Create(user),Times.Once);
+
+        }
+
+        private static User GeAUser()
+        {
+            return new User
+            {
+                Name = "John",
+                LastName = "Kahn",
+                Email = "jjjkahn@gmail.com",
+                Password = "password",
+                EmailConfirmed = "jjjkahn@gmail.com",
+                UserId = Guid.NewGuid()
+            };
+        }
+    }
+}
---
+++ RiverLoggerApi.Tests/Controllers/UserControllerTests.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Moq;
+using NUnit.Framework;
+using RiverLoggerApi.Services;
+
+namespace RiverLoggerApi.Tests.Controllers
+{
+    [TestFixture]
+    public class UserControllerTests
+    {
+        private readonly new Mock<IUserService> _userService;
+        public UserControllerTests(IUserService userService)
+        {
+                _userService = userService;
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+    }
+}
