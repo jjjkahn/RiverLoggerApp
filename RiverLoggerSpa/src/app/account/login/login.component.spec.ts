078eff04b04d1f47cb3ff46e220f9b471d61cddb
Nicolas Thibault <nicolasthibault@hotmail.ca>
2024-05-08 20:33:36 -0400

    added tests

--- RiverLoggerSpa/src/app/account/login/login.component.spec.ts
+++ RiverLoggerSpa/src/app/account/login/login.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,10 +8,13 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      declarations: [LoginComponent],
+      imports: [ReactiveFormsModule]
     })
-    .compileComponents();
-    
+      .compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +22,41 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form invalid when empty', () => {
+    expect(component.loginForm.valid).toBeFalsy();
+  });
+
+  it('email field validity', () => {
+    let email = component.loginForm.controls['email'];
+    expect(email.valid).toBeFalsy();
+
+    let errors: any = {};
+    errors = email.errors || {};
+    expect(errors['required']).toBeTruthy();
+
+    email.setValue("test");
+    errors = email.errors || {};
+    expect(errors['required']).toBeFalsy();
+    expect(errors['email']).toBeTruthy();
+
+    email.setValue("test@example.com");
+    errors = email.errors || {};
+    expect(errors['required']).toBeFalsy();
+    expect(errors['email']).toBeFalsy();
+  });
+
+  it('password field validity', () => {
+    let password = component.loginForm.controls['password'];
+    expect(password.valid).toBeFalsy();
+
+    let errors: any = {};
+    errors = password.errors || {};
+    expect(errors['required']).toBeTruthy();
+
+    password.setValue("123456");
+    errors = password.errors || {};
+    expect(errors['required']).toBeFalsy();
   });
 });
