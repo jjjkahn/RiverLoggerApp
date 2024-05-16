b22ebd1e6182f2b26315e961cc8b3037a6408e73
Jason Kahn <jjjkahn@gmail.com>
2024-05-16 17:44:41 -0400

    Added 2 additional projects for testing

---
+++ RiverLoggerApi.Test/Controllers/AccountControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiverLoggerApi.Test.Controllers
+{
+    internal class AccountControllerTests
+    {
+    }
+}
